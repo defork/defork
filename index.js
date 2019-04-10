97e87c77caa56207275f75302b52de472abca758
johnoro <officialjohnorourke@gmail.com>
2019-04-09 22:58:16 -0400

    Adds current untested progress along with some clarifications

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+const app = new require('@octokit/app')({
+  id: process.env.APP_ID,
+  privateKey: process.env.PRIVATE_KEY
+});
 const octokit = new require('@octokit/rest')();
 const express = require('express');
 const cors = require('cors');
 const server = express();
 
+// for limiting API usage later on if needed
 const DEV_URLS = ['http://localhost:3000'];
 const PROD_URLS = [];
-
 const whitelist = process.env.NODE_ENV === 'production' ? PROD_URLS : DEV_URLS;
-
 const corsOptions = {
   origin: (origin, cb) => {
     // return whitelist.indexOf(origin) !== -1
@@ -26,11 +29,46 @@ server.get('/', (req, res) => {
 
 server.post('/api/defork/:name', async (req, res) => {
   const { name } = req.params;
+  const { toOrg } = req.body;
+  // works for users and orgs
   const response = await octokit.repos.listForUser({
     username: name
   });
+
+  // all forks from supplied user/organization
   const repos = response.data.filter(repo => repo.fork);
-  res.json({ repos });
+
+  const newRepos = [];
+
+  repos.forEach(async repo => {
+    // for initializing new repos in organization
+    const response1 = await octokit.repos.createInOrg({
+      org: toOrg,
+      name: repo.name,
+      description: repo.description
+    });
+
+    console.log(response1);
+
+    // for importing into those new repos
+    // uses public preview https://developer.github.com/v3/previews/#source-import
+    const response2 = await octokit.request('PUT /repos/:owner/:repo/import', {
+      headers: {
+        accept: 'application/vnd.github.barred-rock-preview'
+      },
+      owner: toOrg,
+      repo: repo.name,
+      vcs_url: repo.svn_url,
+      vcs: 'git',
+      mediaType: {
+        previews: ['barred-rock-preview']
+      }
+    });
+
+    newRepos.push(response2.data);
+  });
+
+  res.json({ repos, newRepos });
 });
 
 const PORT = process.env.PORT || 8000;
